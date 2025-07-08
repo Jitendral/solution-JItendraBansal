43b9e612cce3dc7a641328f164e0c8900947bc05
jitendra kumar bansal <40315203+Jitendral@users.noreply.github.com>
2025-07-08 10:28:31 +0530

    Update main.tf

--- assignment/infra/main.tf
+++ assignment/infra/main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_instance" "app" {
+resource "aws_instance" "fullStack-Project" {
   ami                    = "ami-080e1f13689e07408" # Ubuntu 20.04
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
